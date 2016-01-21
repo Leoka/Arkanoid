9d1ed0ec341fed8ece11a0ad5da23b6a21e7f91e
Leoka <majka@autograf.pl>
2016-01-21 14:08:54 +0100

    Adding a bouncing ball

---
+++ src/sample/Ball.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.canvas.GraphicsContext;
+import java.lang.Object;
+
+
+/**
+ * Created by Maja on 21.01.2016.
+ */
+public class Ball {
+
+    private double rx, ry
+}
